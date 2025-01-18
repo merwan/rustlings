ff0df9de79d46a9f99f6bcd9981674ed1fb13bfe
Merouane Atig <merwan@patem.net>
2025-01-18 21:39:31 +0100

    Iterator 4

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -1,4 +1,6 @@
-fn factorial(num: u64) -> u64 {
+use core::iter::Iterator;
+
+fn factorial_recursive(num: u64) -> u64 {
     // TODO: Complete this function to return the factorial of `num` which is
     // defined as `1 * 2 * 3 * … * num`.
     // https://en.wikipedia.org/wiki/Factorial
@@ -10,6 +12,24 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+    match num {
+        0 => 1,
+        1 => 1,
+        _other => num * factorial(num - 1),
+    }
+}
+
+fn factorial_fold(num: u64) -> u64 {
+    #[allow(clippy::unnecessary_fold)]
+    (1..=num).fold(1, |acc, i| acc * i)
+}
+
+fn factorial_product(num: u64) -> u64 {
+    (1..=num).product()
+}
+
+fn factorial(num: u64) -> u64 {
+    factorial_product(num)
 }
 
 fn main() {
